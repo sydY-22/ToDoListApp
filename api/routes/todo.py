65252d2039f10b5ca2a167a5395146af20e32257
Sydney <sydneyb31@me.com>
2026-03-13 14:15:46 -0400

    added routes for api calls and database schema
    Signed-off-by: Sydney <sydneyb31@me.com>

--- api/routes/todo.py
+++ api/routes/todo.py
@@ -1,23 +1,52 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from api.models.todo import Todo
+from api.schemas.todo import GetTodo, CreateTodo, PutTodo
+
 
 todo_router = APIRouter(prefix="/api", tags=["Todo"])
 
+
 @todo_router.get("/")
-def all_todos():
-    return "Not Implemented"
+async def all_todos():
+    return await GetTodo.from_queryset(Todo.all().order_by("-id"))
+
+
+@todo_router.get("/{key}")
+async def get_todo_by_key(key:int):
+    todo = await Todo.get_or_none(id=key)
+
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo Not Found!")
+
+    return await GetTodo.from_tortoise_orm(todo)
 
 
 @todo_router.post("/")
-def post_todo():
-    return "Not Implemented"
+async def create_todo(body: CreateTodo):
+    row = await Todo.create(**body.model_dump(exclude_unset=True))
+    return await GetTodo.from_tortoise_orm(row)
 
 
 @todo_router.put("/{key}")
-def update_todo(key:int):
-    return "Not Implemented"
+async def update_todo(key:int, body:PutTodo):
+    todo = await Todo.get_or_none(id=key)
+
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo Not Found!")
+
+    await todo.update_from_dict(body.model_dump(exclude_unset=True))
+    await todo.save()
+
+    return await GetTodo.from_tortoise_orm(todo)
 
 
 @todo_router.delete("/{key}")
-def delete_todo(key:int):
-    return "Not Implemented"
+async def delete_todo(key:int):
+    todo = await Todo.get_or_none(id=key)
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo Not Found!")
+    await todo.delete()
+    
+    return {"message": "Todo deleted successfully!"}
 
+# test
--- api/schemas/todo.py
+++ api/schemas/todo.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 from tortoise.contrib.pydantic import pydantic_model_creator
+from api.models.todo import Todo
 
 
-GetTodo = pydantic_model_creator(None, name="Todo")
+GetTodo = pydantic_model_creator(Todo, name="Todo")
 
-class PostTodo(BaseModel):
+class CreateTodo(BaseModel):
     task: str = Field(..., max_length=100)
-    done:bool
+    done:bool=False
 
 
 class PutTodo(BaseModel):
